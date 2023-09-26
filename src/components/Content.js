fd3e761867f99176d435eafc848c7fe3fb654816
Alejandro Fuentes <alejandrodfco@gmail.com>
2023-09-26 02:09:50 +0200

    Added firts steps for a backend connection

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import AppHeader from "./AppHeader/AppHeader";
+import PhoneList from "./phoneLists/PhoneList";
+
+import Services from "../services/phoneServices"
 
 const Content = () => {
+
+    const [persons, setPersons] = useState([])
+
+    useEffect ( () => {
+        Services 
+            .get()
+            .then( returnedObjects => {
+                setPersons(returnedObjects);
+            })
+    },[]);
+
     return (
-        <AppHeader />
+        <>
+            <AppHeader />
+            <PhoneList persons={persons}/>
+        </>
     )
 }
 
